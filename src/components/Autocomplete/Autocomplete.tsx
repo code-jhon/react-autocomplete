3986e307e313397aeb7cc7c6783257582e3a6a56
jhon <javilaortiz@gmail.com>
2023-07-17 20:48:11 -0500

    feat: (hightlight) Text highlighted done

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -3,23 +3,34 @@ import './Autocomplete.scss';
 import { AutocompleteProps } from '../../utils/interfaces';
 
 const Autocomplete: React.FC<AutocompleteProps> = ({ options, input }) => {
-
   return (
     <div className='autocomplete'>
       {input && options.length > 0 && (
         <div className='autocomplete__options'>
-          {
-            options.map((option, i) => (
-              <div className='option' key={i}>
-                {option}
-              </div>
-            ))
-          }
-        </div>
-      )
-      }
-    </div>
-  )
-}
+          {options.map((option, i) => {
+            const regex = new RegExp(input, 'gi');
+            const textParts = option.split(regex);
+            const highlightedText = option.match(regex);
 
-export default Autocomplete
+            return (
+              <div className='option' key={i}>
+                {textParts.map((part, index) =>
+                  index < textParts.length - 1 ? (
+                    <React.Fragment key={index}>
+                      {part}
+                      <span className='highlight'>{highlightedText}</span>
+                    </React.Fragment>
+                  ) : (
+                    part
+                  )
+                )}
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Autocomplete;
